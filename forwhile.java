dd176a85840e3c30a8a22eb92af948f2cf7f131e
Aayush Gupta <aayushgupta1003@gmail.com>
2025-08-18 01:31:08 +0530

    Lecture -1 : Basics of Java (if/else,for while and inputOutput,array,string)
    Signed-off-by: Aayush Gupta <aayushgupta1003@gmail.com>

---
+++ forwhile.java
@@ -0,0 +1,14 @@
+public class forwhile {
+    public static void main(String[] args) {
+        // Example usage of for and while loops
+        for (int i = 0; i < 5; i++) {
+            System.out.println("For loop iteration: " + i);
+        }
+
+        int j = 10;
+        while (j < 50) {
+            System.out.println("While loop iteration: " + j);
+            j++;
+        }
+    }
+}
