b3e446a5996b541312aef50346e42ce1dae72809
AAYUSH GUPTA <34982459+Aayush1003@users.noreply.github.com>
2025-09-04 21:56:13 +0530

    Add implementations for basic math algorithms
    Implemented functions and main methods for finding all divisors, checking Armstrong numbers, palindrome numbers, prime numbers, and calculating GCD/HCF in their respective files. These changes provide working examples and user input handling for each algorithm.

--- Basics/Basics_Lecture_Maths/checkPaildrome.java
+++ Basics/Basics_Lecture_Maths/checkPaildrome.java
@@ -1,3 +1,36 @@
+import java.util.Scanner;
+
 public class checkPaildrome {
-    
+                // Example 1:
+                // Input:N = 4554
+               
+                // Output:Palindrome Number
+                
+                // Explanation: The reverse of 4554 is 4554 and therefore it is palindrome number			
+				public static void main(String args[]){
+					
+					
+					Scanner sc = new Scanner(System.in);
+                    int n= sc.nextInt();
+					int rev=n;
+					int nm=0;
+				  
+					while(n!=0){
+						//rev=n/10;
+						int rem=0;
+					rem=n%10;
+						nm=nm*10+rem;
+						n=n/10;
+					}
+					
+	
+				   if (nm==rev){
+                      System.out.println("Number is paildrome");
+				   } else {
+                      System.out.println("Number is not paildrome");
+				   }					   
+					
+					
+				}
+	
 }
--- Basics/Basics_Lecture_Maths/gcdHCF.java
+++ Basics/Basics_Lecture_Maths/gcdHCF.java
@@ -1,3 +1,30 @@
 public class gcdHCF {
-    
+ 
+
+
+
+  // Example 1:
+                // Input:N1 = 9, N2 = 12
+                
+                
+                // Output:3
+                // Explanation:Factors of 9: 1, 3 and 9
+                // Factors of 12: 1, 2, 3, 4, 6, 12
+                // Common Factors: 1, 3 out of which 3 is the greatest hence it is the GCD.
+
+    public static void main(String args[]){
+		int n1=9,n2=12;
+		
+		int gcd=1;
+		for(int i=1; i<= Math.min(n1,n2);i++){
+			
+			if(n1%i==0 && n2%i==0){
+				gcd=i;
+			}
+		}
+		
+		
+		
+		System.out.println("HCF of two numbers "+gcd);
+	}
 }
